17a33063fbbaa308563bc94860fbab39d20f7d6e
CODEVE <codevesoft@proton.me>
2024-10-08 22:11:58 +0200

    Application console for test file generation

--- Source/SpeleoConsole/Program.cs
+++ Source/SpeleoConsole/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
 var path = Path.Combine(@"D:\Veranda", "sample4.txt");
 
 Console.WriteLine("Hello, World!");
@@ -14,6 +18,24 @@ Console.WriteLine("Hello, World!");
 // fsw.Changed += (o, eventArgs) => {Console.WriteLine($"{o.GetType()}{eventArgs.Name} {eventArgs.ChangeType}");};
 // Console.ReadLine();
 
+var interrupteur = new BehaviorSubject<bool>(false);
+var speed = new BehaviorSubject<int>(5);
+interrupteur.AsObservable()
+    .Select(b => b
+        ? speed.AsObservable().Select(theSpeed =>  
+        Observable
+            .Interval(TimeSpan.FromSeconds(theSpeed))
+            .Select(_ => Observable.FromAsync(async () =>
+            {
+                var lines = await File.ReadAllLinesAsync(path);
+                if (int.TryParse(lines.LastOrDefault() ?? "0", out var lastInt))
+                    await File.AppendAllLinesAsync(path, Enumerable.Range(lastInt + 1, 10).Select(i => $"{i}"));
+            }))
+            .Concat())
+            .Switch()
+        : Observable.Return(Unit.Default))
+    .Switch()
+    .Subscribe();
 
 var k = "";
 
@@ -21,11 +43,20 @@ while (k != "x")
 {
     Console.WriteLine("Please enter a number (x to exit):");
     k = Console.ReadLine();
-    if (int.TryParse(k, out var range))
+    if (int.TryParse(k, out var aSpeed))
     {
-        var lines = await File.ReadAllLinesAsync(path);
-        if (int.TryParse(lines.LastOrDefault() ?? "0", out var lastInt))
-            await File.AppendAllLinesAsync(path, Enumerable.Range(lastInt + 1, range).Select(i => $"{i}"));
+        speed.OnNext(aSpeed);
+        continue;
+    }
+
+    switch (k)
+    {
+        case "on":
+            interrupteur.OnNext(true);
+            break;
+        case "off":
+            interrupteur.OnNext(false);
+            break;
     }
 }
 
